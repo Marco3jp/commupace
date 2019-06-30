d1e9a13420b620c3342c60af458b89f1cddf515f
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-01 02:14:36 +0900

    entryでstoreを読むようにしました

--- front/src/scripts/main.ts
+++ front/src/scripts/main.ts
@@ -3,13 +3,13 @@ import Vue from "vue";
 import Vuex from "vuex";
 import Main from "../view/frame.vue";
 import router from "../routes/main";
-//import store from "../store/main";
+import store from "../store/main";
 
 Vue.use(Vuex);
 
 // main vue の初期化
 new Vue({
     router,
-    //store,
+    store,
     render: h => h(Main)
 }).$mount("#app");
