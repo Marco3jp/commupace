85a6703794a30120abb8eaf929e4d2a09355bd72
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-01 02:20:38 +0900

    挙動を確かめるためのテストデータを用意しました

---
+++ front/src/test/mock/chatPostList.ts
@@ -0,0 +1,54 @@
+export let chatPostListMock = [
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "cc0cbf8d-ba12-4181-8671-b92b75d2fc14",
+        postNumber: 1,
+        postUser: {
+            userName: "夢川ゆい",
+            userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517", // ユーザーが決められるようにしてもいいかな？
+            userIcon: "/img/yui.png",
+        },
+        postBody: {
+            text: "さぁ今日もﾕﾒかわにしちゃうぞ(ﾕﾒﾕﾒ",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "f7642fbc-30f5-439a-85c0-dcd7cb00d046",
+        postNumber: 2,
+        postUser: {
+            userName: "夢川ゆい",
+            userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
+            userIcon: "/img/yui.png",
+        },
+        postBody: {
+            text: "足首には鋼鉄が入ってるの！",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "368e09dc-23a2-4c7f-bfa0-a8f3e589b2cb",
+        postNumber: 3,
+        postUser: {
+            userName: "夢川ゆい",
+            userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
+            userIcon: "/img/yui.png",
+        },
+        postBody: {
+            text: "夢川ゆい！ユメかわガッツでユメいきます！",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "6abf90a4-718c-43de-ad75-21d205fb232e",
+        postNumber: 4,
+        postUser: {
+            userName: "夢川ゆい",
+            userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
+            userIcon: "/img/yui.png",
+        },
+        postBody: {
+            text: "ダ ダクラス おいしいご飯は炊く前の準備で決まるの",
+        }
+    }
+];
