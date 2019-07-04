234a4b62bf7f65b6d72448507501cc2f225f1d6b
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-05 00:28:32 +0900

    テスト用データを増やしてタイムスタンプを加えた

--- front/src/test/mock/chatPostList.ts
+++ front/src/test/mock/chatPostList.ts
@@ -3,6 +3,7 @@ export let chatPostListMock = [
         communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
         postId: "cc0cbf8d-ba12-4181-8671-b92b75d2fc14",
         postNumber: 1,
+        postTime: "1562135115421",
         postUser: {
             userName: "夢川ゆい",
             userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517", // ユーザーが決められるようにしてもいいかな？
@@ -16,6 +17,7 @@ export let chatPostListMock = [
         communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
         postId: "f7642fbc-30f5-439a-85c0-dcd7cb00d046",
         postNumber: 2,
+        postTime: "1562245326421",
         postUser: {
             userName: "夢川ゆい",
             userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
@@ -29,6 +31,7 @@ export let chatPostListMock = [
         communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
         postId: "368e09dc-23a2-4c7f-bfa0-a8f3e589b2cb",
         postNumber: 3,
+        postTime: "1562245327421",
         postUser: {
             userName: "夢川ゆい",
             userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
@@ -42,6 +45,7 @@ export let chatPostListMock = [
         communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
         postId: "6abf90a4-718c-43de-ad75-21d205fb232e",
         postNumber: 4,
+        postTime: "1562245337421",
         postUser: {
             userName: "夢川ゆい",
             userId: "afcc905d-9a8b-49c1-a6d9-826d20a2e517",
@@ -50,5 +54,103 @@ export let chatPostListMock = [
         postBody: {
             text: "ダ ダクラス おいしいご飯は炊く前の準備で決まるの",
         }
-    }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "177d2628-d463-4f2f-b2d2-2cd14e300df0",
+        postNumber: 4,
+        postTime: "1562245937421",
+        postUser: {
+            userName: "羽衣ララ",
+            userId: "2d39ede9-8fd8-49d2-ab48-6cdcb2cb7dbd",
+            userIcon: "/img/rara.png",
+        },
+        postBody: {
+            text: "オヨ〜！このサービスは何ルン？　本当に締め切り前に完成するルン？",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "7d9be118-66a7-4e38-991d-e901a0634ab8",
+        postNumber: 4,
+        postTime: "1562245967421",
+        postUser: {
+            userName: "雪城ほのか",
+            userId: "03c04c67-f768-4ec6-b63c-0fac0d6f5d39",
+            userIcon: "/img/honoka.png",
+        },
+        postBody: {
+            text: "ララちゃん、そういうこというのは……ね？",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "45bf2e1e-a1c8-49e4-b18d-094480b03ec0",
+        postNumber: 4,
+        postTime: "1562245997421",
+        postUser: {
+            userName: "南委員長",
+            userId: "a6cae471-8618-4a57-b242-8d73e927e26b",
+            userIcon: "/img/minami_iincho.png",
+        },
+        postBody: {
+            text: "違反チケット！言いたいことははっきり言わなくてはならない！",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "c934c676-9a22-42bb-8feb-aa2f9c14ce4f",
+        postNumber: 4,
+        postTime: "1562246027421",
+        postUser: {
+            userName: "真中らぁら",
+            userId: "e973934a-11e5-4a66-9c9b-e224d554d33a",
+            userIcon: "/img/manaka_laala.png",
+        },
+        postBody: {
+            text: "いいんちょー！ぽっぷすてっぷげっちゅー！って言ってみてくださ〜い！",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "31869e23-9c84-4fa7-8459-d1e47be217cd",
+        postNumber: 4,
+        postTime: "1562246087421",
+        postUser: {
+            userName: "南委員長",
+            userId: "a6cae471-8618-4a57-b242-8d73e927e26b",
+            userIcon: "/img/minami_iincho.png",
+        },
+        postBody: {
+            text: "……",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "bc751846-9f76-4350-94b8-ea065def711d",
+        postNumber: 4,
+        postTime: "1562247287421",
+        postUser: {
+            userName: "南みれぃ",
+            userId: "a6cae471-8618-4a57-b242-8d73e927e26b",
+            userIcon: "/img/mirei.png",
+        },
+        postBody: {
+            text: "ポップステップげっちゅー♪　らぁら、これでいい？",
+        }
+    },
+    {
+        communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
+        postId: "9bf1026e-e7af-43a2-b9ee-bfd3a982cf7b",
+        postNumber: 4,
+        postTime: "1562247307421",
+        postUser: {
+            userName: "らぁら",
+            userId: "e973934a-11e5-4a66-9c9b-e224d554d33a",
+            userIcon: "/img/laala.png",
+        },
+        postBody: {
+            text: "かしこまっ！……って、プリパラワールドに来るなんてずるい〜！",
+        }
+    },
 ];
--- front/src/test/postChat.ts
+++ front/src/test/postChat.ts
@@ -4,6 +4,7 @@ export function postChat(message) {
         communityId: "a6bc86ea-bd09-4b62-a1d8-ff6c05558a35",
         postId: uuidv4(),
         postNumber: 10, // server increment last post number. but this func cannot get last post number.
+        postTime: Date.now(),
         postUser: {
             userName: "You",
             userId: "dc0ab403-6f47-4cac-abb4-0b82f13843cb",
