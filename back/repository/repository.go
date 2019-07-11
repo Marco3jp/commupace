6ef1caefb5965b7fde3c20601f963a827d6c5cab
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-11 16:09:52 +0900

    CommunityAccountRepositoryを追加しました

--- back/repository/repository.go
+++ back/repository/repository.go
@@ -8,3 +8,11 @@ type ManagerAccountRepository interface {
 	Update(newManagerAccount *model.ManagerAccount) error
 	Delete(id uint) error
 }
+
+type CommunityAccountRepository interface {
+	Add(newCommunityAccount model.CommunityAccount) (id uint, err error)
+	FindOne(id uint) (communityAccount *model.CommunityAccount, err error)
+	FindFromManagerAccount(managerAccountId uint) (communityAccount []model.CommunityAccount, err error)
+	Update(newCommunityAccount *model.CommunityAccount) error
+	Delete(id uint) error
+}
