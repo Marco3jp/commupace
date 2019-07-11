e9a2d95cbfef2c2943e88f8e55617270e4937ba2
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-11 16:09:24 +0900

    ManagerAccountのAdd時の引数にManagerAccountのModelを追加しました

--- back/repository/repository.go
+++ back/repository/repository.go
@@ -3,7 +3,7 @@ package repository
 import "../model"
 
 type ManagerAccountRepository interface {
-	Add() (id uint, err error)
+	Add(managerAccount *model.ManagerAccount) (id uint, err error)
 	FindOne(id uint) (managerAccount *model.ManagerAccount, err error)
 	Update(newManagerAccount *model.ManagerAccount) error
 	Delete(id uint) error
