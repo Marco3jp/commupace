705847bbbc359e62497762019dc76fa2695c3f09
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-11 16:15:10 +0900

    locationの座標情報を別途構造体に切り出しました

--- back/model/location.go
+++ back/model/location.go
@@ -4,6 +4,10 @@ import "github.com/jinzhu/gorm"
 
 type Location struct {
 	gorm.Model
+	Coordinates
+}
+
+type Coordinates struct {
 	Latitude  float64 `gorm:"index:lat"`
 	Longitude float64 `gorm:"index:long"`
 }
