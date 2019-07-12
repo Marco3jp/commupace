d299da69320115629cca97005285f1baa6638711
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-13 04:22:13 +0900

    PostRepositoryを実装した

---
+++ back/repository/database/post.go
@@ -0,0 +1,76 @@
+package database
+
+import (
+	"github.com/jinzhu/gorm"
+	".."
+	"../../model"
+)
+
+type PostRepositoryImpl struct {
+	db *gorm.DB
+}
+
+func PostRepository(db *gorm.DB) repository.PostRepository {
+	return &PostRepositoryImpl{db: db}
+}
+
+func (pr *PostRepositoryImpl) Add(newPost *model.Post) (id uint, err error) {
+	if !pr.db.NewRecord(*newPost) {
+		return 0, &repository.IDError{err.Error()}
+	}
+
+	if err := pr.db.Create(newPost).Error; err != nil {
+		return 0, &repository.IOError{err.Error()}
+	}
+
+	return newPost.ID, nil
+}
+
+func (pr *PostRepositoryImpl) FindOne(id uint) (post *model.Post, err error) {
+	if pr.db.First(post, id).RecordNotFound() {
+		return nil, &repository.NotFoundRecordError{"Action: postTable"}
+	}
+
+	return post, nil
+}
+
+func (pr *PostRepositoryImpl) FindFromCommunityId(communityId uint) (posts []model.Post, err error) {
+	if pr.db.Where("community_id = ?", communityId).Find(posts).RecordNotFound() {
+		return nil, &repository.NotFoundRecordError{"Action: postTable"}
+	}
+
+	return posts, nil
+}
+
+// TODO: Threadが実装されたら追加
+// func (pr *PostRepositoryImpl) FindFromThread(threadId uint) (posts []model.Post, err error){}
+
+func (pr *PostRepositoryImpl) FetchRangeFromCommunityId(communityId uint, count uint) (posts []model.Post, err error) {
+	if pr.db.Where("community_id = ?", communityId).Order("post_number desc").Limit(count).Find(posts).RecordNotFound() {
+		return nil, &repository.NotFoundRecordError{"Action: postTable"}
+	}
+
+	return posts, nil
+}
+
+// TODO: Threadが実装されたら追加
+// func (pr *PostRepositoryImpl) FetchRangeFromThread(threadId uint, count uint) (posts []model.Post, err error){}
+
+func (pr *PostRepositoryImpl) Update(newPost *model.Post) error {
+	if err := pr.db.Save(newPost).Error; err != nil {
+		return &repository.IOError{err.Error()}
+	}
+
+	return nil
+}
+
+func (pr *PostRepositoryImpl) Delete(id uint) error {
+	target := model.Post{}
+	target.ID = id
+
+	if err := pr.db.Delete(&target).Error; err != nil {
+		return &repository.IOError{err.Error()}
+	}
+
+	return nil
+}
