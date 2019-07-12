391e6f9a614dca0054af7cfa6e6583ec6f7a450a
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-13 01:29:25 +0900

    命名が誤っていたので修正

--- back/repository/repository.go
+++ back/repository/repository.go
@@ -3,7 +3,7 @@ package repository
 import "../model"
 
 type ManagerAccountRepository interface {
-	Add(managerAccount *model.ManagerAccount) (id uint, err error)
+	Add(newManagerAccount *model.ManagerAccount) (id uint, err error)
 	FindOne(id uint) (managerAccount *model.ManagerAccount, err error)
 	Update(newManagerAccount *model.ManagerAccount) error
 	Delete(id uint) error
