3be766985a1af5f305cff0350d70933e47ab01d5
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-13 22:51:26 +0900

    ManagerAccountModuleを実装

---
+++ back/module/managerAccount.go
@@ -0,0 +1,34 @@
+package module
+
+import (
+	"../repository"
+	"../model"
+)
+
+type ManagerAccountModuleImpl struct {
+	ManagerAccountRepository repository.ManagerAccountRepository
+}
+
+func NewManagerAccountModule(mar *repository.ManagerAccountRepository) ManagerAccountModule {
+	return &ManagerAccountModuleImpl{ManagerAccountRepository: mar}
+}
+
+func (mam *ManagerAccountModuleImpl) CreateManagerAccount() (managerAccountId string, err error) {
+	managerAccountId, err = CreateUUIDWithoutHyphen()
+	if err != nil {
+		return "", err
+	}
+
+	managerAccount := &model.ManagerAccount{
+		ManagerAccountID: managerAccountId,
+		Email:            nil,
+		Password:         nil,
+	}
+
+	_, err = mam.ManagerAccountRepository.Add(managerAccount)
+	if err != nil {
+		return "", err
+	}
+
+	return managerAccountId, nil
+}
