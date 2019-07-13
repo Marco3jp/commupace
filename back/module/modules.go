6b8098c62652cd5b13778c48753d62f4833f006f
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-13 21:49:52 +0900

    TokenModuleを追加した

---
+++ back/module/modules.go
@@ -0,0 +1,10 @@
+package module
+
+type TokenModule interface {
+	CreateToken(managerAccountId string) (accessToken string, refreshToken string, err error)
+	RefreshToken(managerAccountId string, refreshToken string) (newAccessToken string, newRefreshToken string, err error)
+	// AccessTokenが有効かチェックする。
+	// 引数が空など、本来エラーであっても無効であることに変わりはないのでfalseが返る仕様になっている。
+	IsValidAccessToken(accessToken string, managerAccountId string) bool
+	// isValidRefreshToken()
+}
