ddcdbe93498fdf29aaa1295f49dc563b668e6fb7
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-16 04:33:09 +0900

    APIHandlerにchatModuleを追加

--- back/handler/handler.go
+++ back/handler/handler.go
@@ -9,18 +9,21 @@ type APIHandler struct {
 	ManagerAccountModule   module.ManagerAccountModule
 	CommunityAccountModule module.CommunityAccountModule
 	CommunityModule        module.CommunityModule
+	ChatModule             module.ChatModule
 }
 
 func NewAPIHandler(
 	tokenModule module.TokenModule,
 	managerAccountModule module.ManagerAccountModule,
 	communityAccountModule module.CommunityAccountModule,
-	communityModule module.CommunityModule) *APIHandler {
+	communityModule module.CommunityModule,
+	chatModule module.ChatModule) *APIHandler {
 
 	return &APIHandler{
 		TokenModule:            tokenModule,
 		ManagerAccountModule:   managerAccountModule,
 		CommunityAccountModule: communityAccountModule,
 		CommunityModule:        communityModule,
+		ChatModule:             chatModule,
 	}
 }
