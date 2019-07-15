11b549858bb6843cdd72c01634eb50668e6de711
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-15 14:09:24 +0900

    CommunityModuleをAPIHandlerに追加

--- back/handler/handler.go
+++ back/handler/handler.go
@@ -8,12 +8,19 @@ type APIHandler struct {
 	TokenModule            module.TokenModule
 	ManagerAccountModule   module.ManagerAccountModule
 	CommunityAccountModule module.CommunityAccountModule
+	CommunityModule        module.CommunityModule
 }
 
-func NewAPIHandler(tokenModule module.TokenModule, managerAccountModule module.ManagerAccountModule, communityAccountModule module.CommunityAccountModule) *APIHandler {
+func NewAPIHandler(
+	tokenModule module.TokenModule,
+	managerAccountModule module.ManagerAccountModule,
+	communityAccountModule module.CommunityAccountModule,
+	communityModule module.CommunityModule) *APIHandler {
+
 	return &APIHandler{
 		TokenModule:            tokenModule,
 		ManagerAccountModule:   managerAccountModule,
 		CommunityAccountModule: communityAccountModule,
+		CommunityModule:        communityModule,
 	}
 }
