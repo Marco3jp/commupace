1e1dd54f6b84e41bd0844b75f9595aacf1e15510
marco <28888207+Marco3jp@users.noreply.github.com>
2019-07-16 04:32:23 +0900

    ChatModuleを実装

---
+++ back/module/chat.go
@@ -0,0 +1,41 @@
+package module
+
+import (
+	"../repository"
+	"../model"
+)
+
+type ChatModuleImpl struct {
+	PostRepo repository.PostRepository
+}
+
+func NewChatModule(pr repository.PostRepository) ChatModule {
+	return &ChatModuleImpl{PostRepo: pr}
+}
+
+// PostTypeという謎のカラムとPostNumberが空のPost構造体を返します
+func (cm *ChatModuleImpl) CreatePost(communityAccountId uint, communityId uint, postText string, postPath string) model.Post {
+	return model.Post{
+		CommunityAccountID: communityAccountId,
+		CommunityID:        communityId,
+		PostText:           postText,
+		PostPath:           postPath,
+	}
+}
+
+func (cm *ChatModuleImpl) Post(post model.Post) error {
+	_, err := cm.PostRepo.Add(&post)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (cm *ChatModuleImpl) GetPosts(communityId uint, count uint) (posts []model.Post, err error) {
+	posts, err = cm.PostRepo.FetchRangeFromCommunityId(communityId, communityId)
+	if err != nil {
+		return nil, err
+	}
+
+	return posts, nil
+}
